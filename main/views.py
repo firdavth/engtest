25e623b47f6c593fa70ffaa0f4853b6138bab5a2
XAbdulbosit099 <114180502+XAbdulbosit099@users.noreply.github.com>
2023-02-18 19:46:13 +0500

    Update views.py

--- main/views.py
+++ main/views.py
@@ -10,26 +10,6 @@ def Index(request):
 
 
 def Testing(request, pk):
-    tests = []
-    for i in range(10):
-        test = random.choice(Test.objects.filter(category_id=pk))
-        if test in tests:
-            pass
-        else:
-            tests.append(test)
-    return render(request, ".html", {"test": tests})
-
-
-def add_suggestion(request):
-    if request.method == "POST":
-        names = request.POST.get('names')
-        message = request.POST.get('message')
-        Chat.objects.create(names=names, message=message)
-        return render(request,'index.html')
-    return redirect('index')
-
-
-def Results(request):
     if request.method == "POST":
         test = request.POST.getlist("test")
         answer = request.POST.getlist("answer")
@@ -40,5 +20,24 @@ def Results(request):
                 percent += 10
                 result += 1
         return render(request, 'index.html', {'result': result, 'percent': percent})
+    else:
+        tests = []
+        while True:
+            test = random.choice(Test.objects.filter(category_id=pk))
+            count = random.choice([1, 2, 3, 4, 5, 6, 7, 8])
+            if test in tests:
+                pass
+            else:
+                tests.append({"test": test, "count": count})
+            if len(tests) == 10:
+                break
+        return render(request, "test.html", {"tests": tests})
 
 
+def add_suggestion(request):
+    if request.method == "POST":
+        names = request.POST.get('names')
+        message = request.POST.get('message')
+        Chat.objects.create(names=names, message=message)
+        return render(request,'index.html')
+    return redirect('index')
